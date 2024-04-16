5fa9aa7780f4eaabc13e59b6b70f38e3f16f86cc
Morabet <ismailelmorabet2@gmail.com>
2024-04-16 12:50:36 +0000

    task Box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,31 +4,31 @@ Write a method that determines if all the boxes can be opened.
 '''
 
 
-def getKeys(Boxes, keys):
-	'''a recursive function to get all the keys in opened boxes'''
-	updatedKeys = set()
-	updatedKeys.update(keys)
-	set_init_size = len(updatedKeys)
+def getkeys(boxes, keys):
+    '''a recursive function to get all the keys in opened boxes'''
+    updated_keys = set()
+    updated_keys.update(keys)
+    set_init_size = len(updated_keys)
 
-	for k in keys:
-		if k < len(Boxes):
-			updatedKeys.update(Boxes[k])
+    for k in keys:
+        if k < len(boxes):
+            updated_keys.update(boxes[k])
 
-	if set_init_size != len(updatedKeys):
-		return getKeys(Boxes, updatedKeys)
-	else:
-		return updatedKeys
-	
-def canUnlockAll(Boxes):
-	'''check if all Boxes can be opened'''
-	if len(Boxes) == 0:
-		return False
-	
-	keys = getKeys(Boxes, [0])
-	
-	for i in range(len(Boxes)):
-		if i not in keys:
-			return False
-	
-	return True
-	
+    if set_init_size != len(updated_keys):
+        return getkeys(boxes, updated_keys)
+
+    return updated_keys
+
+
+def canUnlockAll(boxes):
+    '''check if all boxes can be opened'''
+    if len(boxes) == 0:
+        return False
+
+    keys = getkeys(boxes, [0])
+
+    for i in range(len(boxes)):
+        if i not in keys:
+            return False
+
+    return True
