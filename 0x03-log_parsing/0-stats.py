8981ea5f8011c1a54f1a09f3afc4371701c50195
Morabet <ismailelmorabet2@gmail.com>
2024-05-02 14:08:42 +0000

    fixing log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 '''Log parsing'''
 
+import sys
 
-from sys import stdin
-
+codes = {
+    '200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+    '404': 0, '405': 0, '500': 0
+    }
 i = 0
 total_size = 0
-codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+
 
 try:
-    for line in stdin:
+    for line in sys.stdin:
         line_list = line.split(" ")
 
         if len(line_list) > 4:
@@ -18,23 +20,25 @@ try:
             file_size = int(line_list[-1])
 
             if status_code in codes.keys():
-                codes[status_code] = codes[status_code] + 1
                 codes[status_code] += 1
 
             total_size += file_size
+
             i += 1
 
-            if i == 10:
-                print('File size: {}'.format(total_size))
-                for code, value in sorted(codes.items()):
-                    if value != 0:
-                        print('{}: {}'.format(code, value))
-                i = 0
+        if i == 10:
+            i = 0
+            print('File size: {}'.format(total_size))
 
-except KeyboardInterrupt:
+            for key, value in sorted(codes.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
     pass
+
 finally:
     print('File size: {}'.format(total_size))
-    for code, value in sorted(codes.items()):
+    for key, value in sorted(codes.items()):
         if value != 0:
-            print('{}: {}'.format(code, value))
+            print('{}: {}'.format(key, value))
