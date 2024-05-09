59e33f6604bd00e49302c4bfb5ef57e4087601c7
Morabet <ismailelmorabet2@gmail.com>
2024-05-09 23:37:17 +0000

    fixing task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,16 +1,42 @@
 #!/usr/bin/python3
-"""  UTF-8 Validation"""
+'''UTF-8 Validation'''
 
 
-def validUTF8(data):
-    """ validatinf if data is a valid utf-8"""
+from typing import List
 
-    for num in data:
-        bin_num = f'{num:08b}'
-        len_bits = len(bin_num)
-        if len_bits == 8 and bin_num[0] != "0":
-            return False
-        if len_bits > 8 and bin_num[-8:-6] != "10":
-            return False
 
-    return True
+def validUTF8(data: List[int]) -> bool:
+    '''validating if a set of data is a valid utf-8'''
+
+    # will count how many more byte needed in case
+    # of more than one byte character
+    byte_needed = 0
+
+    for number in data:
+        if byte_needed == 0:
+
+            if number >> 5 == 0b110 or number >> 5 == 0b1110:
+                byte_needed = 1
+
+            elif number >> 4 == 0b1110:
+                byte_needed = 2
+
+            elif number >> 3 == 0b11110:
+                byte_needed = 3
+
+            elif number >> 7 == 0b1:
+                # invalid case if only one byte and msb = 1
+                # ! all one byte values that start with 0 are valid
+                return False
+
+        else:
+            # if more than one byte then 2 msb must be '10'
+            if number >> 6 != 0b10:
+                return False
+
+            byte_needed -= 1
+
+    # return True
+    # if data contains a single and only one byte long
+    # if in case more than one byte, and has sufficient and valid bytes
+    return byte_needed == 0
