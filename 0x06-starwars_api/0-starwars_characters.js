f285dbeaa9d62e5243f910d1738bdac9fba4372a
Morabet <ismailelmorabet2@gmail.com>
2024-05-23 15:41:06 -0700

    fix semi

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,42 @@
 #!/usr/bin/node
- /**
+/**
   * Star Wars Characters
 */
-
 
 const request = require('request');
 
 const lastArg = process.argv[process.argv.length - 1];
-const urlFilm = "https://swapi-api.alx-tools.com/api/films/" + lastArg;
+const urlFilm = 'https://swapi-api.alx-tools.com/api/films/' + lastArg;
 
 const makeRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            if (error) {
-                reject(error);
-            } else if (response.statusCode !== 200) {
-                reject(new Error('Failed to load page, status code: ' + response.statusCode));
-            } else {
-                resolve(JSON.parse(body));
-            }
-        });
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(new Error('Failed to load page, status code: ' + response.statusCode));
+      } else {
+        resolve(JSON.parse(body));
+      }
     });
+  });
 };
 
 const fetchFilm = () => {
-    return makeRequest(urlFilm)
-        .then(data => data['characters']);
+  return makeRequest(urlFilm)
+    .then(data => data.characters);
 };
 
 const fetchChar = () => {
-    return fetchFilm()
-        .then(urls => {
-            if (!urls) {
-                throw new Error('No URLs found');
-            }
-            const charPromises = urls.map(url => makeRequest(url).then(character => character['name']));
-            return Promise.all(charPromises);
-        });
+  return fetchFilm()
+    .then(urls => {
+      if (!urls) {
+        throw new Error('No URLs found');
+      }
+      const charPromises = urls.map(url => makeRequest(url).then(character => character.name));
+      return Promise.all(charPromises);
+    });
 };
-
 
 const main = async () => {
   try {
@@ -50,6 +48,6 @@ const main = async () => {
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 main();
