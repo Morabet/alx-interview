f9c42ef4c266960c23dc4577064e898e0d0c9445
Morabet <ismailelmorabet2@gmail.com>
2024-06-11 10:59:58 -0700

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,17 +4,17 @@
 
 def island_perimeter(grid):
     """ calculate the perimeter of the island"""
-    n = len(grid)
+    rows = len(grid)
+    cols = len(grid[0])
     perimeter = 0
-    for i in range(n):
-        for j in range(n):
+
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
-                if j > n and grid[i][j + 1] == 0:
-                    perimeter += 1
-                if j > 0 and grid[i][j - 1] == 0:
-                    perimeter += 1
-                if i > n and grid[i + 1][j] == 0:
-                    perimeter += 1
-                if i > 0 and grid[i - 1][j] == 0:
-                    perimeter += 1
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
     return perimeter
